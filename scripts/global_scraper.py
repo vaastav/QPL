eced44ce65551512ed7c2914b4840aa003ad454f
vaastav <vaastav.anand05@gmail.com>
2017-07-08 17:17:34 -0700

    Update scripts to parse and clean all data

--- scripts/global_scraper.py
+++ scripts/global_scraper.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from utility import uprint
-from parsers import parse_players
+from parsers import *
 
 # Retrieves data from the hard-coded url
 # that contains the data
@@ -13,8 +13,23 @@ def get_data():
     data = json.loads(responseStr)
     return data
 
+def get_individual_player_data(player_id):
+    base_url = "https://fantasy.premierleague.com/drf/element-summary/"
+    full_url = base_url + str(player_id)
+    response = requests.get(full_url)
+    if response.status_code != 200:
+        raise Exception("Response was code " + str(response.status_code))
+    data = json.loads(response.text)
+    return data
+
 def parse_data(data):
-    parse_players(data["elements"])
+    #parse_players(data["elements"])
+    # TODO: parse other stats that may be useful
+    num_players = len(data["elements"])
+    for i in range(num_players):
+        player_data = get_individual_player_data(i+1)
+        parse_player_history(player_data["history_past"], i+1)
+        parse_player_gw_history(player_data["history"], i+1)
 
 data = get_data()
 parse_data(data)
