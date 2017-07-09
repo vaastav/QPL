38ae68112f7ea93ca46aa51bc47c39876ae7016f
vaastav <vaastav.anand05@gmail.com>
2017-07-09 00:29:54 -0700

    Add user registration form

--- qplsite/qpl/views.py
+++ qplsite/qpl/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+
+from qplsite.qpl.forms import SignUpForm
 
 def index(request):
     return HttpResponse("Hello, world. You're at the QPL index.")
+
+@login_required
+def home(requres):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
