36b9dca9cc3a82cb41cb7e4ba214340788f56dcb
vaastav <vaastav.anand05@gmail.com>
2017-07-09 23:49:32 -0700

    Fix typos related to signup

--- qplsite/qplsite/urls.py
+++ qplsite/qplsite/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     url(r'^login/', auth_views.login,  {'template_name': 'login.html'}, name='login'),
     url(r'^logout/', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'', qpl_views.home, name='home'),
-    url(r'^singup/', qpl_views.signup, name='signup'),
+    url(r'^signup/', qpl_views.signup, name='signup'),
 ]
