c87ca15aada23f4db6dde9f60200cd19a798006f
vaastav <vaastav.anand05@gmail.com>
2017-07-10 00:01:40 -0700

    Fix timezone and home link

--- qplsite/qplsite/urls.py
+++ qplsite/qplsite/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', auth_views.login,  {'template_name': 'login.html'}, name='login'),
     url(r'^logout/', auth_views.logout, {'next_page': 'login'}, name='logout'),
-    url(r'', qpl_views.home, name='home'),
+    url(r'^$', qpl_views.home, name='home'),
     url(r'^signup/', qpl_views.signup, name='signup'),
 ]
